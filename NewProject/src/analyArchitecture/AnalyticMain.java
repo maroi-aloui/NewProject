97a66089d2f5e3bdf502d6c67b7244e23a35078a
cc491188 <cc491188@C31697.ens.uqam.ca>
2017-03-30 12:35:28 -0400

    commit7

--- NewProject/src/analyArchitecture/AnalyticMain.java
+++ NewProject/src/analyArchitecture/AnalyticMain.java
@@ -6,6 +6,7 @@ import org.restlet.resource.ClientResource;
 
 import analyArchitecture.RessourceApplication;
 
+
 import java.io.FileWriter;
 import java.lang.reflect.Field;
 
@@ -18,7 +19,7 @@ public class AnalyticMain {
 
 	    // Add a new HTTP server listening on port 8183.  
 	    
-	    component.getServers().add(Protocol.HTTP, 8182);  
+	    component.getServers().add(Protocol.HTTP, 8482);  
 	  
 	    // Attach the sample application.  
 	    component.getDefaultHost().attach("/analyArchitecture",  
@@ -26,30 +27,43 @@ public class AnalyticMain {
 
 	    // Start the component.  
 	    component.start();  
-	     
-	  /* Debut generate Logs
+	    
+	    /*
 	    Logs obj = new Logs();
+	    @SuppressWarnings("resource")
+		FileWriter file = new FileWriter("c:/Logs/file2.json");
+	    file.write(obj.getNomServeur());
+        file.flush();
+        	*/
+        
+	   //Debut generate Logs
+	     Logs obj = new Logs();
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields)
         {
             field.setAccessible(true);
             System.out.println(field.getName() + " -> " + field.get(obj));
-        }
-	     Fin LOgs*/
-	   
+            FileWriter file = new FileWriter("c:/Logs/file3.json");
+	    	file.write((int) field.get(obj));
+        	file.flush();
+        }  
+	    // Fin LOgs
+	    
+
+	     /*
 	    Gson gson = new Gson();
 	    Logs obj = new Logs();
 
 		// 1. Java object to JSON, and save into a file
 		gson.toJson(obj, new FileWriter("c:/Logs/file.json"));
-
+	   	*/
 	  
 	    }
 	   
 	    catch (Exception e){
 	    	System.out.println(e.toString());
 	    }
-	    
+	   
 	    //client
 	    ClientResource resource = new ClientResource ("http://localhost:8183/analyArchitecture/LogIngestor");
 	    resource.put(new Object ()).write(System.out);
