2953a481d5878cfb9ff3713a326ac1c7bfed4d2b
cc491188 <cc491188@C31697.ens.uqam.ca>
2017-03-30 11:34:13 -0400

    commit7

--- NewProject/src/analyArchitecture/AnalyticMain.java
+++ NewProject/src/analyArchitecture/AnalyticMain.java
@@ -5,17 +5,20 @@ import org.restlet.data.Protocol;
 import org.restlet.resource.ClientResource;
 
 import analyArchitecture.RessourceApplication;
+
+import java.io.FileWriter;
 import java.lang.reflect.Field;
 
 public class AnalyticMain {
 	public static void main(String[] args) throws Exception {  
 	    try{
+	    	
 	    // Create a new Component.  
 	    Component component = new Component();  
 
 	    // Add a new HTTP server listening on port 8183.  
 	    
-	    component.getServers().add(Protocol.HTTP, 8183);  
+	    component.getServers().add(Protocol.HTTP, 8182);  
 	  
 	    // Attach the sample application.  
 	    component.getDefaultHost().attach("/analyArchitecture",  
@@ -23,27 +26,35 @@ public class AnalyticMain {
 
 	    // Start the component.  
 	    component.start();  
-	    
-	    //Debut generate Logs
+	     
+	  /* Debut generate Logs
 	    Logs obj = new Logs();
-	    
         Field[] fields = obj.getClass().getDeclaredFields();
         for (Field field : fields)
         {
             field.setAccessible(true);
             System.out.println(field.getName() + " -> " + field.get(obj));
         }
-	    //Fin LOgs
-	    
-	    
+	     Fin LOgs*/
+	   
+	    Gson gson = new Gson();
+	    Logs obj = new Logs();
+
+		// 1. Java object to JSON, and save into a file
+		gson.toJson(obj, new FileWriter("c:/Logs/file.json"));
+
+	  
 	    }
+	   
 	    catch (Exception e){
+	    	System.out.println(e.toString());
 	    }
 	    
 	    //client
 	    ClientResource resource = new ClientResource ("http://localhost:8183/analyArchitecture/LogIngestor");
 	    resource.put(new Object ()).write(System.out);
 	
+	  
 	}
 	
 
